59ca8176c9930a89f8da920e63581792506c52a9
Michael R Creel <michaelrcreel@gmail.com>
2024-12-20 13:46:08 -0600

    feat(left menu): add profile card to homepage left menu

--- src/app/components/ProfileCard.tsx
+++ src/app/components/ProfileCard.tsx
@@ -18,6 +18,38 @@ const ProfileCard = () => {
           className="absolute -bottom-6 left-0 right-0 m-auto h-12 w-12 rounded-full ring-1 ring-white"
         />
       </div>
+      <div className="flex h-20 flex-col items-center gap-2">
+        <span className="font-semibold">Lura Fisher</span>
+        <div className="flex items-center gap-4">
+          <div className="flex">
+            <Image
+              src="https://images.pexels.com/photos/29743753/pexels-photo-29743753/free-photo-of-portrait-of-woman-in-sunlit-tropical-setting.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+              alt="avatar"
+              width={12}
+              height={12}
+              className="h-3 w-3 rounded-full object-cover"
+            />
+            <Image
+              src="https://images.pexels.com/photos/29743753/pexels-photo-29743753/free-photo-of-portrait-of-woman-in-sunlit-tropical-setting.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+              alt="avatar"
+              width={12}
+              height={12}
+              className="h-3 w-3 rounded-full object-cover"
+            />
+            <Image
+              src="https://images.pexels.com/photos/29743753/pexels-photo-29743753/free-photo-of-portrait-of-woman-in-sunlit-tropical-setting.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+              alt="avatar"
+              width={12}
+              height={12}
+              className="h-3 w-3 rounded-full object-cover"
+            />
+          </div>
+          <span className="text-xs text-gray-500">621 Followers</span>
+        </div>
+        <button className="rounded-md bg-blue-500 p-2 text-xs text-white">
+          My profile
+        </button>
+      </div>
     </div>
   )
 }
